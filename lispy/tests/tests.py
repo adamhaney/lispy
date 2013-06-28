1a7ce56926fb4db1821ce92deb44b7e8c3550ba3
Adam Haney <adam.haney@akimbo.io>
2013-06-27 22:12:44 -0400

    * Added and fixed a few tests

--- lispy/tests/tests.py
+++ lispy/tests/tests.py
@@ -1,4 +1,6 @@
-from unittest2 import TestCase
+from unittest import TestCase
+
+from lispy.runtime import Runtime
 
 
 class SimpleTest(TestCase):
@@ -8,5 +10,11 @@ class SimpleTest(TestCase):
     def test_simple_addition(self):
         self.assertEquals(
             self.runtime.eval("(+ 2 2)"),
-            2
+            4
+        )
+
+    def test_simple_module_access(self):
+        import os
+        self.assertEquals(
+            self.runtime.eval("(list os:environ)"), [os.environ]
         )
