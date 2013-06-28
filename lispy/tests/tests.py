aaddc045c9dac64001bd684cfcaa4e89f4e8e9f1
Adam Haney <adam.haney@akimbo.io>
2013-06-28 14:33:58 -0400

    * Finally got 'doctest' module working so the SICP examples can be worked through

--- lispy/tests/tests.py
+++ lispy/tests/tests.py
@@ -1,13 +1,20 @@
+import re
+import os
+import doctest
 from unittest import TestCase
 
-from sh import lispy
+import sh
+from sh import lispy, grep
 
 from lispy.runtime import Runtime
 
-
-class SimpleEvaluationTest(TestCase):
+class RuntimeTest(TestCase):
     def setUp(self):
         self.runtime = Runtime()
+        self.test_directory = os.path.dirname(os.path.realpath(__file__))
+
+
+class SimpleEvaluationTest(RuntimeTest):
 
     def test_simple_addition(self):
         self.assertEquals(
@@ -22,17 +29,14 @@ class SimpleEvaluationTest(TestCase):
         )
 
 
-class PythonBuiltinsTest(TestCase):
-
-    def setUp(self):
-        self.runtime = Runtime()
+class PythonBuiltinsTest(RuntimeTest):
 
     def test_py_builtin_abs(self):
         self.assertEquals(
             self.runtime.eval("(py:abs -7)"), 7
         )
 
-class CommandlineTest(TestCase):
+class CommandlineTest(RuntimeTest):
     """
     Test the options provided by lispy's cli using python's sh module
     """
@@ -42,3 +46,20 @@ class CommandlineTest(TestCase):
         being evaluated properly
         """
         self.assertEquals(lispy("-c (+ 2 2)"), "4\n")
+
+class SICPTest(RuntimeTest):
+    def test_sicp_examples(self):
+        """
+        Test all the files in the SICP code directory
+        """
+
+        chapters_dir = "{}/code/sicp/chapters".format(self.test_directory)
+
+        for lispy_file in os.listdir(chapters_dir):
+            abs_script_path = "{}/{}".format(chapters_dir, lispy_file)
+            output = lispy("{}".format(abs_script_path))
+            comments = re.findall(";;.*", open(abs_script_path).read())
+            expected = map(lambda line: line.replace(";;", ""), comments)
+
+            for output, expected in zip(output.split('\n'), expected):
+                self.assertEquals(expected.strip(), output)
