8feb95e446d70d1571188ec067fd0b14b3ca1d7f
Adam Haney <adam.haney@akimbo.io>
2013-07-03 01:09:38 -0400

    * PEP8 for tests

--- lispy/tests/tests.py
+++ lispy/tests/tests.py
@@ -91,7 +91,8 @@ lis_tests = [
     ("((repeat twice) 5)", 20), ("((repeat (repeat twice)) 5)", 80),
     ("(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1))))))", None),
     ("(fact 3)", 6),
-    ("(fact 50)", 30414093201713378043612608166064768844377641568960512000000000000),
+    ("(fact 50)",
+     30414093201713378043612608166064768844377641568960512000000000000),
     ("(define abs (lambda (n) ((if (> n 0) + -) 0 n)))", None),
     ("(list (abs -3) (abs 0) (abs 3))", [3, 0, 3]),
     ("""(define combine (lambda (f)
@@ -102,14 +103,20 @@ lis_tests = [
     ("(define zip (combine cons))", None),
     ("(zip (list 1 2 3 4) (list 5 6 7 8))", [[1, 5], [2, 6], [3, 7], [4, 8]]),
     ("""(define riff-shuffle (lambda (deck) (begin
-    (define take (lambda (n seq) (if (<= n 0) (quote ()) (cons (car seq) (take (- n 1) (cdr seq))))))
+    (define take
+      (lambda (n seq)
+        (if (<= n 0)
+          (quote ()) (cons (car seq) (take (- n 1) (cdr seq))))))
     (define drop (lambda (n seq) (if (<= n 0) seq (drop (- n 1) (cdr seq)))))
     (define mid (lambda (seq) (/ (length seq) 2)))
-    ((combine append) (take (mid deck) deck) (drop (mid deck) deck)))))""", None),
+    ((combine append) (take (mid deck) deck) (drop (mid deck) deck)))))""",
+     None),
     ("(riff-shuffle (list 1 2 3 4 5 6 7 8))", [1, 5, 2, 6, 3, 7, 4, 8]),
-    ("((repeat riff-shuffle) (list 1 2 3 4 5 6 7 8))",  [1, 3, 5, 7, 2, 4, 6, 8]),
-    ("(riff-shuffle (riff-shuffle (riff-shuffle (list 1 2 3 4 5 6 7 8))))", [1,2,3,4,5,6,7,8]),
-    ]
+    ("((repeat riff-shuffle) (list 1 2 3 4 5 6 7 8))",
+     [1, 3, 5, 7, 2, 4, 6, 8]),
+    ("(riff-shuffle (riff-shuffle (riff-shuffle (list 1 2 3 4 5 6 7 8))))",
+     [1, 2, 3, 4, 5, 6, 7, 8]),
+]
 
 lispy_tests = [
     ("()", SyntaxError),
@@ -125,7 +132,7 @@ lispy_tests = [
     ("(twice 2)", 4),
     ("(twice 2 2)", TypeError),
     ("(define lyst (lambda items items))", None),
-    ("(lyst 1 2 3 (+ 2 2))", [1,2,3,4]),
+    ("(lyst 1 2 3 (+ 2 2))", [1, 2, 3, 4]),
     ("(if 1 2)", 2),
     ("(if (= 3 4) 2)", None),
     ("(define ((account bal) amt) (set! bal (+ bal amt)) bal)", None),
@@ -142,23 +149,34 @@ lispy_tests = [
     ("(= (square-root 200.) (sqrt 200.))", True),
     ("""(define (sum-squares-range start end)
          (define (sumsq-acc start end acc)
-            (if (> start end) acc (sumsq-acc (+ start 1) end (+ (* start start) acc))))
+            (if
+              (> start end)
+                acc
+                (sumsq-acc (+ start 1) end (+ (* start start) acc))))
          (sumsq-acc start end 0))""", None),
-    ("(sum-squares-range 1 3000)", 9004500500), ## Tests tail recursion
+    ("(sum-squares-range 1 3000)", 9004500500),  # Tests tail recursion
     ("(call/cc (lambda (throw) (+ 5 (* 10 (throw 1))))) ;; throw", 1),
     ("(call/cc (lambda (throw) (+ 5 (* 10 1)))) ;; do not throw", 15),
     ("""(call/cc (lambda (throw)
-         (+ 5 (* 10 (call/cc (lambda (escape) (* 100 (escape 3)))))))) ; 1 level""", 35),
+         (+ 5 (* 10 (call/cc
+                      (lambda (escape)
+                        (* 100 (escape 3)))))))) ; 1 level""", 35),
     ("""(call/cc (lambda (throw)
-         (+ 5 (* 10 (call/cc (lambda (escape) (* 100 (throw 3)))))))) ; 2 levels""", 3),
+         (+ 5 (* 10 (call/cc
+                      (lambda (escape) (* 100 (throw 3)))))))) ; 2 levels""",
+     3),
     ("""(call/cc (lambda (throw)
-         (+ 5 (* 10 (call/cc (lambda (escape) (* 100 1))))))) ; 0 levels""", 1005),
+         (+ 5 (* 10 (call/cc (lambda (escape) (* 100 1))))))) ; 0 levels""",
+     1005),
     ("(* 1i 1i)", -1), ("(sqrt -1)", 1j),
     ("(let ((a 1) (b 2)) (+ a b))", 3),
     ("(let ((a 1) (b 2 3)) (+ a b))", SyntaxError),
     ("(and 1 2 3)", 3), ("(and (> 2 1) 2 3)", 3), ("(and)", True),
     ("(and (> 2 1) (> 2 3))", False),
-    ("(define-macro unless (lambda args `(if (not ,(car args)) (begin ,@(cdr args))))) ; test `", None),
+    ("""(define-macro unless
+          (lambda args
+            `(if (not ,(car args)) (begin ,@(cdr args))))) ; test `""",
+     None),
     ("(unless (= 2 (+ 1 1)) (display 2) 3 4)", None),
     (r'(unless (= 4 (+ 1 1)) (display 2) (display "\n") 3 4)', 4),
     ("(quote x)", 'x'),
@@ -166,14 +184,14 @@ lispy_tests = [
     ("'x", 'x'),
     ("'(one 2 3)", ['one', 2, 3]),
     ("(define L (list 1 2 3))", None),
-    ("`(testing ,@L testing)", ['testing',1,2,3,'testing']),
-    ("`(testing ,L testing)", ['testing',[1,2,3],'testing']),
+    ("`(testing ,@L testing)", ['testing', 1, 2, 3, 'testing']),
+    ("`(testing ,L testing)", ['testing', [1, 2, 3], 'testing']),
     ("`,@L", SyntaxError),
     ("""'(1 ;test comments '
      ;skip this line
      2 ; more ; comments ; ) )
-     3) ; final comment""", [1,2,3]),
-    ]
+     3) ; final comment""", [1, 2, 3]),
+]
 
 
 def check_norvig_expected(x, expected, rt):
